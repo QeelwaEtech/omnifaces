48fc2d423bd0daf63bbe3e88605d2ef2327b9245
Bauke Scholtz <Bauke Scholtz@localhost>
2013-02-28 12:12:59 -0400

    #119: added UnmappedResourceHandler and fixed minor URL path issue in CombinedResource (whose problem exposes only when being used in combination with another resource handler)

---
+++ src/org/omnifaces/facesviews/FacesViewsInitializerListener.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2012 OmniFaces.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+package org.omnifaces.facesviews;
+
+import static org.omnifaces.facesviews.FacesViews.FACES_VIEWS_ENABLED_PARAM_NAME;
+import static org.omnifaces.facesviews.FacesViews.FACES_VIEWS_RESOURCES_EXTENSIONS;
+import static org.omnifaces.facesviews.FacesViews.mapFacesServlet;
+import static org.omnifaces.util.Faces.getApplicationAttribute;
+import static org.omnifaces.util.Utils.isEmpty;
+
+import java.util.Set;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.annotation.WebListener;
+
+import org.omnifaces.eventlistener.DefaultServletContextListener;
+
+
+/**
+ * Convenience class for Servlet 3.0 users, which will map the FacesServlet to extensions found
+ * during scanning in {@link FacesViewsInitializer}. This part of the initialization is executed
+ * in a separate ServletContextListener, because the FacesServlet has to be available. This is
+ * not guaranteed to be the case in an ServletContainerInitializer.
+ * <p>
+ * For a guide on FacesViews, please see the <a href="package-summary.html">package summary</a>.
+ *
+ * @author Arjan Tijms
+ *
+ */
+@WebListener
+public class FacesViewsInitializerListener extends DefaultServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent context) {
+
+        ServletContext servletContext = context.getServletContext();
+        
+        if (!"false".equals(servletContext.getInitParameter(FACES_VIEWS_ENABLED_PARAM_NAME))) {
+        	
+        	Set<String> extensions = getApplicationAttribute(servletContext, FACES_VIEWS_RESOURCES_EXTENSIONS);
+        	
+        	if (!isEmpty(extensions)) {
+        		mapFacesServlet(servletContext, extensions);
+        	}
+        }
+    }
+
+}
+
